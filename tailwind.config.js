1c06895c97b92c8dbdfcce776e656adaa50a461f
https://github.com/sidocoder/first-intern <sifhoran2024@gmail.com>
2025-08-15 10:13:17 +0300

    folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{ts,js,tsx,jsx}",
+    "./src/app/components/**/*.{ts,js,tsx,jsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
